6adaaa64a31939e38da98449742ab5e1366c1fd9
ernestzhang <ernestzhang@vip.qq.com>
2012-04-17 02:14:19 +0800

    append

--- src/appendlog.h
+++ src/appendlog.h
@@ -8,7 +8,8 @@
 
 #ifndef __APPENDLOG_H_
 #define __APPENDLOG_H_
-#define BLOCK_SIZE (32*1024)
+#define BLOCK_SIZE 32
+//(32*1024)
 #define MAX_NODE_SIZE (1024 * 1024)
 enum{
    ERROR_CREATE_FILE_ERROR = -1000,
@@ -48,11 +49,11 @@ typedef struct _StListNode
     struct _StListNode *pNext ;
 }StListNode;
 
-typedef struct StLogNodeList
+typedef struct _StLogNodeList
 {
 	StListNode *pHead ;
     StListNode *pTail ;
-}_StLogNodeList;
+}StLogNodeList;
 
 typedef struct _StLogFile
 {
@@ -67,15 +68,15 @@ typedef struct _StLogFile
 }StLogFile;
 int CreateLogFile(const char *sFile , StLogFile *pLogFile);
 long ReadN(int iFd , char *cBuf ,  long lSize);
-int WriteN(int iFd , char *cBuf , unsigned int uiSize , unsigned int *uiTotal);
-int AppendLog(StLogFile *pLogFile);
-unsigned int GenCheckSum(void *pData , unsigned int uiSize);
+int WriteN(int iFd , char *cBuf , unsigned long uiSize , unsigned int *uiTotal);
+int AppendLog(StLogFile *pLogFile , int iFlush);
+unsigned int GenCheckSum(void *pData , unsigned long uiSize);
 void PackKvNode(char *pBuf , StLogNode *pNode);
-void PackLogData(StLogFile *pLogFile , char *pData , unsigned int uiSize , char cType);
+void PackLogData(StLogFile *pLogFile , char *pData , unsigned long uiSize , char cType);
 int AppendRecord(StLogFile *pLogFile , StLogNode*pNode);
 int IsUnValidDataRecord(StLogRecordNode *pNode , unsigned long ulSize);
 StLogNode *CreateNewNode(StLogFile *pLogFile , StLogNode *pData);
-int ParseLogData(StLogFile *pLogFile , void *pData , unsigned long ulSize);
+int ParseLogData(StLogFile *pLogFile , void *pData , unsigned long ulSize , char cType);
 int ProcessLogFile(StLogFile*pLogFile);
 int GetLogData(StLogFile *pLogFile , char *sFile);
 int DestroyFile(StLogFile *pLogFile);
