3a40cac7afa4f428de38154ef1b2a07df5a46700
ernestzhang <ernestzhang@vip.qq.com>
2012-04-30 15:56:12 +0800

    sstable

--- src/memtable.h
+++ src/memtable.h
@@ -1,8 +1,19 @@
 #ifndef __MEMTABLE_INCLUDE
 #define __MEMTABLE_INCLUDE
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 #include "skiplist.h"
 typedef struct _StMemTable
 {
 	SkipList *pList ;
+	SkipList_func_equal funcCompare ;
 }StMemTable;
+
+int memtable_init(StMemTable *pstTab , SkipList_func_equal Compare);
+int memtable_set(StMemTable *pstTab , StValNode *pNode);
+StValNode * memtable_find(StMemTable *pstTab , char *sKey );
+int memtable_del(StMemTable *pstTab ,char *sKey  );
+
 #endif
--- src/sstable.h
+++ src/sstable.h
@@ -1,11 +1,33 @@
 #ifndef __SSTABLE_INCLUDE_
 #define __SSTABLE_INCLUDE_
 
+#define MAX_KEY_SIZE 256
+
+typedef struct _StTabNode
+{
+	unsigned int uiCrc32 ;
+	StValNode *pNode ;
+}StTabNode;
+
+typedef struct _StSSTIndex
+{
+	char cMinKey[MAX_KEY_SIZE];
+	char cMaxKey[MAX_KEY_SIZE];
+	long lIndexStart ;
+	long lIndexSize ;
+	long lIndexCnt ;
+	unsigned int uiCrc32;
+	StTabNode  **pNode ;
+}StSSTIndex;
+
 typedef struct _StSstable
 {
-	int iFd ;
+	int  iFileID ; 
+	int  iFd ;
+ 	long lFileSize ;
 	
 }StSstable;
+
 
 
 
